9d41b9a6a1db049645bc85454cb049e9e4d52631
GLAS Red <GLAbSas@outlook.com>
2025-01-25 16:10:08 +0500

    Добавлены данные для ТС (необходимо поправить ввод данных о ТС и сделать удаление)

---
+++ SmartLoad/Migrations/20250122143712_UpdateVehicleTypeWithKind.cs
@@ -0,0 +1,205 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartLoad.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateVehicleTypeWithKind : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AllowedRoadTypes",
+                table: "VehicleTypes",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "AxleCount",
+                table: "VehicleTypes",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<float>(
+                name: "CouplingDevice",
+                table: "VehicleTypes",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<float>(
+                name: "EmptyWeight",
+                table: "VehicleTypes",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<float>(
+                name: "FrontOverhang",
+                table: "VehicleTypes",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<float>(
+                name: "Height",
+                table: "VehicleTypes",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<float>(
+                name: "Kingpindist",
+                table: "VehicleTypes",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<float>(
+                name: "Length",
+                table: "VehicleTypes",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<float>(
+                name: "MaxAxleLoad",
+                table: "VehicleTypes",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<float>(
+                name: "MaxLoadCapacity",
+                table: "VehicleTypes",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<float>(
+                name: "MaxVolumeCapasity",
+                table: "VehicleTypes",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Notes",
+                table: "VehicleTypes",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<float>(
+                name: "OverBeyond",
+                table: "VehicleTypes",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<float>(
+                name: "RearOverhang",
+                table: "VehicleTypes",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ViewType",
+                table: "VehicleTypes",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<float>(
+                name: "WheelBase",
+                table: "VehicleTypes",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<float>(
+                name: "Width",
+                table: "VehicleTypes",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AllowedRoadTypes",
+                table: "VehicleTypes");
+
+            migrationBuilder.DropColumn(
+                name: "AxleCount",
+                table: "VehicleTypes");
+
+            migrationBuilder.DropColumn(
+                name: "CouplingDevice",
+                table: "VehicleTypes");
+
+            migrationBuilder.DropColumn(
+                name: "EmptyWeight",
+                table: "VehicleTypes");
+
+            migrationBuilder.DropColumn(
+                name: "FrontOverhang",
+                table: "VehicleTypes");
+
+            migrationBuilder.DropColumn(
+                name: "Height",
+                table: "VehicleTypes");
+
+            migrationBuilder.DropColumn(
+                name: "Kingpindist",
+                table: "VehicleTypes");
+
+            migrationBuilder.DropColumn(
+                name: "Length",
+                table: "VehicleTypes");
+
+            migrationBuilder.DropColumn(
+                name: "MaxAxleLoad",
+                table: "VehicleTypes");
+
+            migrationBuilder.DropColumn(
+                name: "MaxLoadCapacity",
+                table: "VehicleTypes");
+
+            migrationBuilder.DropColumn(
+                name: "MaxVolumeCapasity",
+                table: "VehicleTypes");
+
+            migrationBuilder.DropColumn(
+                name: "Notes",
+                table: "VehicleTypes");
+
+            migrationBuilder.DropColumn(
+                name: "OverBeyond",
+                table: "VehicleTypes");
+
+            migrationBuilder.DropColumn(
+                name: "RearOverhang",
+                table: "VehicleTypes");
+
+            migrationBuilder.DropColumn(
+                name: "ViewType",
+                table: "VehicleTypes");
+
+            migrationBuilder.DropColumn(
+                name: "WheelBase",
+                table: "VehicleTypes");
+
+            migrationBuilder.DropColumn(
+                name: "Width",
+                table: "VehicleTypes");
+        }
+    }
+}
--- SmartLoad/Models/VehicleType.cs
+++ SmartLoad/Models/VehicleType.cs
@@ -5,5 +5,57 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        
+        //Длина ТС в метрах
+        public float Length { get; set; }
+
+        //Ширина ТС 
+        public float Width { get; set; }
+
+        //Высота ТС
+        public float Height { get; set; }
+
+        //Максимальная грузоподъёмность в кг
+        public float MaxLoadCapacity { get; set; }
+
+        //Максимальный объём 
+        public float MaxVolumeCapasity { get; set; }
+
+        //Количество осей 
+        public int AxleCount { get; set; }
+
+        //Максимально допустимая нагрузка на одну ось
+        public float MaxAxleLoad { get; set; }
+
+        //Собственный вес ТС
+        public float EmptyWeight { get; set; }
+
+        //База колёс (растояние между передней и задней осями, в метрах)
+        public float WheelBase { get; set; }
+
+        //Передний свес 
+        public float FrontOverhang { get; set; }
+
+        //Задний свес 
+        public float RearOverhang { get; set; }
+
+        //Тип дорог, по которым может передвигаться ТС (например, городские, междугородние, горные)
+        public string AllowedRoadTypes { get; set; }
+
+        //Дополнительные примечасния или особенности
+        public string Notes { get; set; }
+
+        //Вид ТС
+        public string ViewType { get; set; }
+
+        //Высота седельно-сцепного устройства (важна для полуприцепа)
+        public float CouplingDevice { get; set; }
+
+        // Расстояние от переднего свеса до шкворня. Это расстояние влияет на маневренность полуприцепа.
+        public float Kingpindist { get; set; }
+
+        // Задний свес за седельно-сцепным устройством, важен для расчета распределения нагрузки на оси.
+        public float OverBeyond { get; set; }
+
     }
 }
